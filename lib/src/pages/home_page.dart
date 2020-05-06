30edae81105dbb3f514a5b45a1fe745d8c6e3093
Alejandro Alamar <alejaam@users.noreply.github.com>
2020-05-05 22:21:37 -0500

    Creando paginas home y settings, y manejo de rutas

---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+
+  static final String routeName = 'home';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Preferencias de usuario'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text('Color secundario:'),
+          Divider(),
+          Text('Género:'),
+          Divider(),
+          Text('Nombre de usuario:'),
+          Divider(),
+        ],
+      ),
+    );
+  }
+}
